e983b69ae415f846810ff465c053c12ecea49bee
Krabbyz <82833156+Krabbyz@users.noreply.github.com>
2024-10-02 22:57:01 -0700

    Session 1 Prompt for Coding Journal 1.

---
+++ cj1-1.py
@@ -0,0 +1,10 @@
+def Print():
+	print("My name is Austin Nguyen. ")
+	print("My favorite movie is The Hobbit, An Unexpected Journey. ")
+	print("My favorite food is pasta.")
+
+def main():
+	Print()
+
+if __name__=="__main__":
+	main()
