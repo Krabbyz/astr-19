a9624cea6523dae0bb38c8a9c9a30820a9df9fd3
Krabbyz <82833156+Krabbyz@users.noreply.github.com>
2024-10-02 23:57:53 -0700

    Session 2 prompt for Coding Journal 1

---
+++ cj1-2.py
@@ -0,0 +1,17 @@
+def main():
+	a = 5
+	b = 10
+	c = 10.0
+	d = 5.0
+	x = c+d
+	print(x)
+	print(type(x))
+	x = b-a
+	print(x)
+	print(type(x))
+	x = c*b
+	print(x)
+	print(type(x))
+
+if __name__=="__main__":
+	main()
